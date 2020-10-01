fc9613ee9137a0b8eed2353226499ecc6cf1d203
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2020-10-01 16:35:50 +0300

    Добавлено описание 8-й квартиры

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@
 const map = document.querySelector(`.map`);
 const pinTemplate = document.querySelector(`#pin`).content.querySelector(`.map__pin`);
 const ADVERTISEMENTS_AMOUNT = 8;
-const TITLES = [`Просторная светлая квартира`, `Маленькая грязная квартира`, `Подводный лофт для экстремалов`, `Шикарный зимний дворец`, `Каюта на затонувшем корабле`, `Бунгало в центре города`, `Картонная коробка эконом класса`];
-const PRICES = [0, 10000, 20000, 5000, 70000, 60000, 1000000, 5];
+const TITLES = [`Просторная светлая квартира`, `Маленькая грязная квартира`, `Подводный лофт для экстремалов`, `Шикарный зимний дворец`, `Каюта на затонувшем корабле`, `Бунгало в центре города`, `Картонная коробка эконом класса`, `Старый дом с богатой историей для семьи`];
+const PRICES = [0, 10000, 20000, 5000, 70000, 60000, 1000000, 5, 10000];
 const TYPES = [`palace`, `flat`, `house`, `bungalow`];
-const AMOUNT_ROOMS = [1, 3, 32, 1, 4, 2, 17];
-const AMOUNT_GUESTS = [100, 2, 2, 2, 2, 60, 3];
+const AMOUNT_ROOMS = [1, 3, 32, 1, 4, 2, 1, 7];
+const AMOUNT_GUESTS = [100, 2, 2, 2, 2, 60, 3, 7];
 const CHECKS = [`12:00`, `13:00`, `14:00`];
 const FEATURES = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
-const DESCRIPTIONS = [`Без детей, животных, амбиций и планов на жизнь`, `Плюсы тараканов в том, что вам не будет одиноко, минусы, аренду они не платят`, `Мокро, холодно, неудобно, но крайне необычно`, `Счета за отопление соизмеримы разве что с вашим эго`, `Красивые фото обеспечены, но говорят, там водятся призраки`, `Стандартное бунгало, соломенная крыша, выход к океану`, `Очень уютная коробка, но немного продувает и менты гоняют`];
+const DESCRIPTIONS = [`Без детей, животных, амбиций и планов на жизнь`, `Плюсы тараканов в том, что вам не будет одиноко, минусы - аренду они не платят`, `Мокро, холодно, неудобно, но крайне необычно`, `Счета за отопление соизмеримы разве что с вашим эго`, `Красивые фото обеспечены, но говорят, там водятся призраки`, `Стандартное бунгало, соломенная крыша, выход к океану`, `Очень уютная коробка, но немного продувает и менты гоняют`, `Не верьте росказням, что все предыдущие владельцы погибли при мистических обстоятельствах`];
 const PHOTOS = [`http://o0.github.io/assets/images/tokyo/hotel1.jpg`, `http://o0.github.io/assets/images/tokyo/hotel2.jpg`, `http://o0.github.io/assets/images/tokyo/hotel3.jpg`];
 
 const getRandomInt = (min, max) => {
@@ -42,7 +42,7 @@ const renderAdvertisements = () => {
   const advertisements = [];
   const numbers = shuffleArr(createArr(ADVERTISEMENTS_AMOUNT));
   for (let i = 0; i < ADVERTISEMENTS_AMOUNT; i++) {
-    let randomRoom = getRandomInt(0, TITLES.length);
+    let randomRoom = getRandomInt(0, ADVERTISEMENTS_AMOUNT);
     let x = getRandomInt(0, map.offsetWidth);
     let y = getRandomInt(130, 630);
     advertisements[i] = {
