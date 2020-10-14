e0bde57f164e6b16b91565259a7a544ff2d64bb7
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2020-10-15 00:09:58 +0300

    Исправлены координаты в smallPin

--- js/pin.js
+++ js/pin.js
@@ -1,12 +1,12 @@
 'use strict';
 (()=>{
   const getPin = (advertisement) => {
+    const PIN_WIDTH = 50;
+    const PIN_HEIGHT = 70;
     const pinTemplate = document.querySelector(`#pin`).content.querySelector(`.map__pin`);
     let newPin = pinTemplate.cloneNode(true);
     const pinImg = newPin.querySelector(`img`);
-    const pinWidth = Number(pinImg.getAttribute(`width`));
-    const pinHeight = Number(pinImg.getAttribute(`height`));
-    newPin.style = `left: ${advertisement.location.x - pinWidth / 2}px; top: ${advertisement.location.y + pinHeight}px`;
+    newPin.style = `left: ${advertisement.location.x - PIN_WIDTH / 2}px; top: ${advertisement.location.y - PIN_HEIGHT}px`;
     newPin.setAttribute(`data-id`, advertisement.id);
     pinImg.setAttribute(`src`, advertisement.author.avatar);
     pinImg.setAttribute(`alt`, advertisement.offer.title);
