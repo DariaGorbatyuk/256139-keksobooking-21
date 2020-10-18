94fee3a15939748ae5886cd2cb2eac1ac1ca4e57
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2020-10-18 11:37:44 +0300

    main в IIFE

--- js/main.js
+++ js/main.js
@@ -1,22 +1,25 @@
 'use strict';
-const onMainPinClick = (evt) => {
-  if (evt.button !== 0) {
-    return;
-  }
-  if (!window.mods.isActiveTrue) {
+(()=>{
+  const onMainPinClick = (evt) => {
+    if (evt.button !== 0) {
+      return;
+    }
+    if (!window.mods.isActiveTrue) {
+      window.mods.setActiveMode();
+    }
+    window.moving.recalculateCoords(evt, window.data.mainPin, window.render.pinsContainer);
+    window.data.mainPin.removeEventListener(`keydown`, onMainPinPressEnter);
+  };
+  const onMainPinPressEnter = (evt)=>{
+    if (evt.key !== `Enter`) {
+      return;
+    }
     window.mods.setActiveMode();
-  }
-  window.moving.recalculateCoords(evt, window.data.mainPin, window.render.pinsContainer);
-  window.data.mainPin.removeEventListener(`keydown`, onMainPinPressEnter);
-};
-const onMainPinPressEnter = (evt)=>{
-  if (evt.key !== `Enter`) {
-    return;
-  }
-  window.mods.setActiveMode();
-  window.data.mainPin.removeEventListener(`keydown`, onMainPinPressEnter);
-};
-window.data.mainPin.addEventListener(`mousedown`, onMainPinClick);
-window.data.mainPin.addEventListener(`keydown`, onMainPinPressEnter);
-window.mods.setPassiveMode();
+    window.data.mainPin.removeEventListener(`keydown`, onMainPinPressEnter);
+  };
+  window.data.mainPin.addEventListener(`mousedown`, onMainPinClick);
+  window.data.mainPin.addEventListener(`keydown`, onMainPinPressEnter);
+  window.mods.setPassiveMode();
+})();
+
 
