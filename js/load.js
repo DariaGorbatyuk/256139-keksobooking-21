e1e40f7de7715fc0e6f309aade7362be604b01f6
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2020-10-20 23:57:45 +0300

    Вставляется элемент с ошибкой

--- js/load.js
+++ js/load.js
@@ -1,8 +1,10 @@
 'use strict';
 (()=>{
-  const URL = `https://21.javascript.pages.academy/keksobooking/data`;
+  // const URL = `https://21.javascript.pages.academy/keksobooking/data`;
+  const URL = `!`;
   const xhr = new XMLHttpRequest();
   xhr.responseType = `json`;
+
   const onSuccess = (data)=>{
     window.load = {
       advertisements: data
@@ -10,6 +12,11 @@
   };
   const onError = (error)=>{
     console.error(error);
+    let node = document.createElement(`div`);
+    node.style = `z-index: 100; margin: 0 auto; background-color: gold; position: absolute; left: 0; right: 0`;
+    node.style.fontSize = `30px`;
+    node.textContent = error;
+    window.data.map.insertAdjacentElement(`afterbegin`, node);
   };
   xhr.addEventListener(`load`, ()=>{
     let error;
@@ -24,7 +31,7 @@
         error = `Пользователь не авторизован`;
         break;
       case 404:
-        error = `Ничего не найдено`;
+        error = `Запрашиваемый ресурс не найден`;
         break;
       case 500:
         error = `Внутренняя ошибка сервера`;
