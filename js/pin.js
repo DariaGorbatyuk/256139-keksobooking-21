0aa5276df3a533466e766a24c228e4e1a4f83727
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2020-10-21 11:43:27 +0300

    Исправлены id в pin

--- js/pin.js
+++ js/pin.js
@@ -7,7 +7,7 @@
     let newPin = pinTemplate.cloneNode(true);
     const pinImg = newPin.querySelector(`img`);
     newPin.style = `left: ${advertisement.location.x - PIN_WIDTH / 2}px; top: ${advertisement.location.y - PIN_HEIGHT}px`;
-    newPin.dataset.id = i + 1;
+    newPin.dataset.id = i;
     pinImg.src = advertisement.author.avatar;
     pinImg.alt = advertisement.offer.title;
     return newPin;
