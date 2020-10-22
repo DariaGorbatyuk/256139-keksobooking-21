6cbe86e496f2f7fe07216892c73eeac9379b1a0f
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2020-10-22 21:04:12 +0300

    удален файл main

--- js/mode.js
+++ js/mode.js
@@ -1,14 +1,44 @@
 'use strict';
 (() => {
   const pinsContainer = window.map.pinsContainer;
-  // move to move
-  let startCoords = {x: 0, y: 0};
+  let startCoords = {x: 0, y: 0}; // мне кажется не правильно что я в одном файле и координаты считаю и режимы задаю?
 
   const setStateForTags = (tags, state) => {
     tags.forEach((item) => {
       item.disabled = state;
     });
   };
+  const setPassive = () => {
+    window.mode.isActive = false;
+    window.data.map.classList.add(`map--faded`);
+    window.form.adForm.classList.add(`ad-form--disabled`);
+    setStateForTags(window.form.adFieldsets, true);
+    setStateForTags(window.form.filterSelects, true);
+    window.form.setNewAddress(true);
+    window.data.mainPin.addEventListener(`click`, onMainPinActive);
+    window.data.mainPin.addEventListener(`keydown`, onMainPinActive);
+    window.data.mainPin.addEventListener(`mousedown`, onMainPinMouseDown);
+  };
+
+  const setActive = () => {
+    window.mode.isActive = true;
+    setStateForTags(window.form.adFieldsets, false);
+    setStateForTags(window.form.filterSelects, false);
+    window.data.map.classList.remove(`map--faded`);
+    window.form.adForm.classList.remove(`ad-form--disabled`);
+    window.map.renderPinsList(window.download.advertisements);
+    window.form.adAddress.readOnly = true;
+    window.form.verifyRoomsCapacity();
+    window.form.verifyPriceForNight();
+    pinsContainer.addEventListener(`click`, window.map.onSmallPinActivated);
+    pinsContainer.addEventListener(`keydown`, window.map.onSmallPinActivated);
+    window.form.adTypeOfHousing.addEventListener(`change`, window.form.onChangeAdTypeOfHousing);
+    window.form.adRoomNumber.addEventListener(`change`, window.form.onChangeAdRoomCapacity);
+    window.form.adRoomCapacity.addEventListener(`change`, window.form.onChangeAdRoomCapacity);
+    window.form.timeIn.addEventListener(`change`, window.form.onTimeChange);
+    window.form.timeOut.addEventListener(`change`, window.form.onTimeChange);
+  };
+
   const onMainPinMouseDown = (evt)=>{
     startCoords = getCoords(evt);
     window.map.pinsContainer.addEventListener(`mousemove`, onMouseMove);
@@ -46,48 +76,11 @@
       return;
     }
     if (!window.mode.isActive) {
-      window.mode.setActive();
+      setActive();
     }
     window.data.mainPin.removeEventListener(`keydown`, onMainPinActive);
     window.data.mainPin.removeEventListener(`click`, onMainPinActive);
   };
 
-  const setPassive = () => {
-    window.mode.isActive = false;
-    window.data.map.classList.add(`map--faded`);
-    window.form.adForm.classList.add(`ad-form--disabled`);
-    setStateForTags(window.form.adFieldsets, true);
-    setStateForTags(window.form.filterSelects, true);
-    window.form.setNewAddress(true);
-    window.data.mainPin.addEventListener(`click`, onMainPinActive);
-    window.data.mainPin.addEventListener(`keydown`, onMainPinActive);
-    window.data.mainPin.addEventListener(`mousedown`, onMainPinMouseDown);
-  };
-
-  const setActive = () => {
-    window.mode.isActive = true;
-    setStateForTags(window.form.adFieldsets, false);
-    setStateForTags(window.form.filterSelects, false);
-    window.data.map.classList.remove(`map--faded`);
-    window.form.adForm.classList.remove(`ad-form--disabled`);
-    window.map.renderPinsList(window.download.advertisements);
-    window.form.adAddress.readOnly = true;
-    window.form.verifyRoomsCapacity();
-    window.form.verifyPriceForNight();
-    pinsContainer.addEventListener(`click`, window.map.onSmallPinActivated);
-    pinsContainer.addEventListener(`keydown`, window.map.onSmallPinActivated);
-    window.form.adTypeOfHousing.addEventListener(`change`, window.form.onChangeAdTypeOfHousing);
-    window.form.adRoomNumber.addEventListener(`change`, window.form.onChangeAdRoomCapacity);
-    window.form.adRoomCapacity.addEventListener(`change`, window.form.onChangeAdRoomCapacity);
-    window.form.timeIn.addEventListener(`change`, window.form.onTimeChange);
-    window.form.timeOut.addEventListener(`change`, window.form.onTimeChange);
-  };
-
-
-  window.mode = {
-    setPassive,
-    setActive,
-    isActive: false
-  };
-
+  setPassive();
 })();
