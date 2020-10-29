244db4a8ed1c2b1e3a7980a12e6a9f356b2610bd
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2020-10-29 22:32:54 +0300

    Работают все фильтры кроме чекбоксов

--- js/filter-form.js
+++ js/filter-form.js
@@ -9,8 +9,7 @@
   const housingRooms = filterForm.querySelector(`#housing-rooms`);
   const housingGuests = filterForm.querySelector(`#housing-guests`);
   const housingFeatures = filterForm.querySelector(`#housing-features`);
-
-  const getPrice = (price)=>{
+  const getPrice = (price) => {
     if (price <= MIN_PRICE) {
       return `low`;
     } else if (MIN_PRICE < price < MAX_PRICE) {
@@ -19,9 +18,79 @@
       return `high`;
     }
   };
+  const isSelected = (items) => {
+    items.forEach((item, i) => {
+      keys[i].selected = item.value !== `any`;
+    });
+  };
+  const keys = [
+    {
+      name: `housingType`,
+      filter: housingType,
+      selected: false,
+      path: `type`
+    },
+    {
+      name: `housingPrice`,
+      filter: housingPrice,
+      selected: false,
+      path: `price`
+    },
+    {
+      name: `housingRooms`,
+      filter: housingRooms,
+      selected: false,
+      path: `rooms`
+    },
+    {
+      name: `housingGuests`,
+      filter: housingGuests,
+      selected: false,
+      path: `guests`
+    }
+  ];
+
+  const onFilterChange = () => {
+    debugger;
+    const selectors = [housingType, housingPrice, housingRooms, housingGuests];
+    isSelected(selectors);
+
+    let keysSelectedFilter = keys.filter((i) => i.selected);
+    let adv = window.download.advertisements;
+    if (keysSelectedFilter.length !== 0) {
+      adv = adv.filter((item) => {
+        let res = [];
+        keysSelectedFilter.forEach((key) => {
+          let advValue = item.offer[key.path];
+          if (key.name === housingPrice) {
+            advValue = getPrice(item.offer[key.path]);
+          }
+          let filterValue = key.filter.value;
+          if (filterValue === String(advValue)) {
+            res.push(true);
+          } else {
+            res.push(false);
+          }
+        });
+        return res.indexOf(false) === -1;
+      });
+    }
+    keysSelectedFilter = [];
+    window.form.deletePinsAndCard();
+    window.map.renderPinsList(adv);
+    window.filterForm.advertisements = adv;
+  };
+  filterForm.addEventListener(`change`, onFilterChange);
 
 
-  const getRank = (adv)=>{
+  window.filterForm = {
+    filterForm,
+    filterSelects
+  };
+})();
+/*
+  */
+/*  const getRank = (adv)=>{
     let rank = 0;
     let maxRank = 0;
     if (adv.offer.type === housingType.value) {
@@ -36,9 +105,9 @@
     if (String(adv.offer.guests) === housingGuests.value) {
       rank++;
     }
-    /*  if (evt.target.value === `any`) { // спорно
+    /!*  if (evt.target.value === `any`) { // спорно
       rank++;
-    }*/
+    }*!/
     if (rank > maxRank) {
       maxRank = rank;
     }
@@ -60,39 +129,7 @@
     adv = adv.filter((item)=>{
       return item.rank === maxRank;
     });
-    /*   adv = adv.filter((item)=>{
-
-      return item.rank > 0;
-    });*/
-
-
     window.form.deletePinsAndCard();
     window.map.renderPinsList(adv);
     window.filterForm.advertisements = adv;
-  };
-  filterForm.addEventListener(`change`, onFilterChange);
-  /*  housingType.addEventListener(`change`, onFilterChange);
-  housingPrice.addEventListener(`change`, onFilterChange);
-  housingRooms.addEventListener(`change`, onFilterChange);
-  housingGuests.addEventListener(`change`, onFilterChange);*/
-
-
-  window.filterForm = {
-    filterForm,
-    filterSelects
-  };
-})();
-
-/*  const findAdv = () => {
-    let newAdvertisements = window.download.advertisements.filter((item) => {
-      return item.offer.type === housingType.value;
-    });
-    window.filterForm.advertisements = newAdvertisements;
-    return newAdvertisements;
-  };
-
-  const onHouseTypeChange = () => {
-    let newAdvertisements = findAdv();
-    window.form.deletePinsAndCard();
-    window.map.renderPinsList(newAdvertisements);
   };*/
